6035e13a8073252fbd38b9cf38daaa49f2b9b039
tongkai chen <tongkaichen@tongkaideair.mynetworksettings.com>
2025-09-29 00:19:36 -0400

    Initial commit: cookiecutter data science scaffold (INST414)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='INST414 Project',
+    author='Tongkai Chen',
+    license='MIT',
+)
